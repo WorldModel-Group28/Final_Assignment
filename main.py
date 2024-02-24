c26d7e8d526e9c7b7d42b24b403febcc4bd35fbb
橋口 貴 <takashihashiguchi@Taka-Book-Air.local>
2024-02-24 17:17:18 +0900

    テスト実施

--- main.py
+++ main.py
@@ -348,7 +348,7 @@ def get_html_state(opt, states):
 ########## CLIP用 ################################################## 
 def load_clip_model():
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        model, preprocess = clip.load("ViT-B/32", device=device)
+        model, preprocess = clip.load("ViT-L/14@336px", device=device)
         return model, preprocess, device
 
 def predict_choice(model, preprocess, device, image_path, text_descriptions):
